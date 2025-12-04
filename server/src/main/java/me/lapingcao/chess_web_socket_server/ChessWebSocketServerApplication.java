0057ee446ee044cdcbb7030f4d952b5584c8de1d
Gabe Lapingcao <mgultra2018@gmail.com>
2025-12-03 22:39:33 -0800

    Initialized Spring WebSocket server

---
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/ChessWebSocketServerApplication.java
@@ -0,0 +1,13 @@
+package me.lapingcao.chess_web_socket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessWebSocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessWebSocketServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/me/lapingcao/chess_web_socket_server/ChessWebSocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lapingcao.chess_web_socket_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessWebSocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
