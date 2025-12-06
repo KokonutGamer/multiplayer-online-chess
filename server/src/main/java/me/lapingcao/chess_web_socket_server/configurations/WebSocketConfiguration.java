383172bdc83d2456daf8f18f7a396aeb82463c71
Gabe Lapingcao <mgultra2018@gmail.com>
2025-12-05 17:20:14 -0800

    Implemented web socket configuration on the backend

---
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/configurations/WebSocketConfiguration.java
@@ -0,0 +1,24 @@
+package me.lapingcao.chess_web_socket_server.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://*localhost:[*]");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://*localhost:[*]").withSockJS();
+    }
+}
