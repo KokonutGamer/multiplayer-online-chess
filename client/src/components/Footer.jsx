ea7e35f8d10aacef6f5fcdf651f8093ec8942c10
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-25 14:14:08 -0700

    Hotfixed HTML classes by changing JSX attributes to className

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,13 +3,13 @@ function Footer() {
         <section>
             <h2>About us</h2>
             <p>This platform was built for fun by two students at the University of Wasington, Bothell.</p>
-            <ul class="actions">
-                <li><a href="#" class="button">Learn More</a></li>
+            <ul className="actions">
+                <li><a href="#" className="button">Learn More</a></li>
             </ul>
         </section>
         <section>
             <h2>Contact Info</h2>
-            <dl class="alt">
+            <dl className="alt">
                 <dt>Address</dt>
                 <dd>17927 113th Ave NE &bull; Bothell, WA &bull; 98011</dd>
                 <dt>Phone</dt>
