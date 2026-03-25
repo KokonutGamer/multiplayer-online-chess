39fd9ff7bf2f01c31051f7160b1d7895fa2a113d
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-25 14:46:15 -0700

    Implemented test function for sending messages to the server

--- client/src/pages/OnlinePlayPage.jsx
+++ client/src/pages/OnlinePlayPage.jsx
@@ -1,41 +1,70 @@
 import { Client } from "@stomp/stompjs"
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function OnlinePlayPage() {
+    const stompClientRef = useRef(null)
+    const [isConnected, setIsConnected] = useState(false)
+
     useEffect(() => {
         const stompClient = new Client({
             brokerURL: `ws://${import.meta.env.VITE_API_DOMAIN}/ws`,
             connectHeaders: {
                 userId: 'abc'
             },
-            debug: (msg) => {
-                console.log(`STOMP debug: ${msg}`)
-            },
+            debug: (msg) => console.log(`STOMP debug: ${msg}`),
             reconnectDelay: 5000,
-            heartbeatIncoming: 4000,
-            heartbeatOutgoing: 4000
+            heartbeatIncoming: 20000,
+            heartbeatOutgoing: 20000
         })
 
+        // TODO within this onConnect property, we will generate a UUID for this user
         stompClient.onConnect = (frame) => {
+            setIsConnected(true)
             console.log("Connected")
             console.log(`Additional details: ${frame.body}`)
         }
 
+        stompClient.onWebSocketError = (error) => {
+            console.error("Error with websocket", error)
+        }
+
         stompClient.onStompError = (frame) => {
-            console.log(`Broker reported error: ${frame.headers['message']}`)
-            console.log(`Additional details: ${frame.body}`)
+            console.error(`Broker reported error: ${frame.headers['message']}`)
+            console.error(`Additional details: ${frame.body}`)
+        }
+
+        stompClient.onDisconnect = () => {
+            setIsConnected(false)
         }
 
         // establish a connection to the server
         stompClient.activate()
+        stompClientRef.current = stompClient
 
+        // clean up STOMP connection
         return async () => {
-            await stompClient.deactivate()
+            if (stompClientRef.current) {
+                await stompClientRef.current.deactivate()
+            }
         }
-    })
+    }, []) // runs only mount
+
+    const hostRequest = () => {
+        if (stompClientRef.current && stompClientRef.current.connected) {
+            stompClientRef.current.publish({
+                destination: '/app/game/host',
+                body: JSON.stringify({
+                    userId: "054be67d-d05f-4187-a75b-601d4c9d5339" // TODO part of client connect
+                })
+            })
+        }
+    }
 
     return (
-        <h1>Play online</h1>
+        <div>
+            <h1>Play online</h1>
+            <button onClick={hostRequest}></button>
+        </div>
     )
 }
 
