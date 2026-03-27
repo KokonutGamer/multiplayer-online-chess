2d615667b3eec1ee64bffe71fece19761a80b9f1
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-26 23:03:14 -0700

    Implemented join request record

--- server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/GameController.java
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/GameController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.lapingcao.chess_web_socket_server.messages.CancelRequest;
 import me.lapingcao.chess_web_socket_server.messages.HostRequest;
+import me.lapingcao.chess_web_socket_server.messages.JoinRequest;
 import me.lapingcao.chess_web_socket_server.messages.MoveRequest;
 import me.lapingcao.chess_web_socket_server.repositories.GameStateRepository;
 
@@ -31,22 +32,22 @@ public class GameController {
     public void cancelGame(@DestinationVariable UUID gameId, @Payload CancelRequest cancelMessage) {
         log.debug("Cancel game request for game with ID {} for user {}", gameId, cancelMessage.userId());
     }
-    
+
     @MessageMapping("{gameId}/join")
-    public void joinGame(@DestinationVariable UUID gameId) {
-        log.debug("Join game request for game with ID {}", gameId);
+    public void joinGame(@DestinationVariable UUID gameId, @Payload JoinRequest joinMessage) {
+        log.debug("Join game request for game with ID {} from user {}", gameId, joinMessage.userId());
     }
-    
+
     @MessageMapping("{gameId}/move")
     public void movePiece(@DestinationVariable UUID gameId, @Payload MoveRequest moveRequest) {
         log.debug("Move piece request for game with ID {}", gameId);
     }
-    
+
     @MessageMapping("{gameId}/draw")
     public void drawGame(@DestinationVariable UUID gameId) {
         log.debug("Draw game request for game with ID {}", gameId);
     }
-    
+
     @MessageMapping("{gameId}/resign")
     public void resignGame(@DestinationVariable UUID gameId) {
         log.debug("Resign game request for game with ID {}", gameId);
