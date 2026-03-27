08ae341679a51ea2c63af50e7559ca2ec7d90837
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-26 22:58:25 -0700

    Outlined all game controller methods with destination mappings

--- server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/GameController.java
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/GameController.java
@@ -1,5 +1,8 @@
 package me.lapingcao.chess_web_socket_server.controllers;
 
+import java.util.UUID;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.lapingcao.chess_web_socket_server.messages.HostRequest;
+import me.lapingcao.chess_web_socket_server.repositories.InMemoryGameStateRepository;
 
 @Controller
 @Slf4j
@@ -14,8 +18,35 @@ import me.lapingcao.chess_web_socket_server.messages.HostRequest;
 @MessageMapping("game")
 public class GameController {
 
+    private final InMemoryGameStateRepository gameStateRepository;
+
     @MessageMapping("host")
     public void hostGame(@Payload HostRequest hostMessage) {
         log.debug("Host request from user {}", hostMessage.userId());
+    }
+
+    @MessageMapping("{gameId}/cancel")
+    public void cancelGame(@DestinationVariable UUID gameId) {
+        log.debug("Cancel game request for game with ID {}", gameId);
+    }
+    
+    @MessageMapping("{gameId}/join")
+    public void joinGame(@DestinationVariable UUID gameId) {
+        log.debug("Join game request for game with ID {}", gameId);
+    }
+    
+    @MessageMapping("{gameId}/move")
+    public void movePiece(@DestinationVariable UUID gameId) {
+        log.debug("Move piece request for game with ID {}", gameId);
+    }
+    
+    @MessageMapping("{gameId}/draw")
+    public void drawGame(@DestinationVariable UUID gameId) {
+        log.debug("Draw game request for game with ID {}", gameId);
+    }
+    
+    @MessageMapping("{gameId}/resign")
+    public void resignGame(@DestinationVariable UUID gameId) {
+        log.debug("Resign game request for game with ID {}", gameId);
     }
 }
