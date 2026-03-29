d5c9a3bea57d9ec3581f4db281bc3709c0e1e580
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-29 16:00:18 -0700

    Hotfixed env.properties class path issues and refactored user controller to generate JWTs instead of UUIDs

--- server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/UserController.java
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/controllers/UserController.java
@@ -4,22 +4,35 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.lapingcao.chess_web_socket_server.services.JwtVerifierService;
 
+/**
+ * TODO document UserController @RestController
+ */
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("users")
 @CrossOrigin(originPatterns = { "http://*localhost:[*]" }, methods = { RequestMethod.GET })
 public class UserController {
 
-    // TODO use an in-memory set to check if a UUID exists; will need to change this to a POST later since this should not be idempotent
-    @GetMapping("generate-id")
-    public Map<String, UUID> getAvailableId() {
-        return Map.of("userId", UUID.randomUUID());
+    /**
+     * TODO document jwtVerifierService member
+     */
+    private final JwtVerifierService jwtVerifierService;
+
+    // TODO use an in-memory set to check if a UUID exists; will need to change this
+    // to a POST later since this should not be idempotent
+    @PostMapping("generate-id")
+    public Map<String, String> getAvailableId() {
+        UUID newUserId = UUID.randomUUID();
+        return Map.of("accessToken", jwtVerifierService.signUser(newUserId));
     }
 }
