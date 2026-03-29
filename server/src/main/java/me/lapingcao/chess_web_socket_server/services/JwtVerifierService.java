c5c125b16bba7c6f27764a1e6613159e085b298c
Gabe Lapingcao <mgultra2018@gmail.com>
2026-03-29 00:44:58 -0700

    Implemented JWT verifier service for parsing JWTs

---
+++ server/src/main/java/me/lapingcao/chess_web_socket_server/services/JwtVerifierService.java
@@ -0,0 +1,39 @@
+package me.lapingcao.chess_web_socket_server.services;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+/**
+ * TODO document JwtVerifierService @Service
+ */
+@Service
+public class JwtVerifierService {
+
+    /**
+     * TODO document jwtSecret member
+     */
+    @Value("${postgres.jwt.secret}")
+    private String jwtSecret;
+
+    /**
+     * TODO document verify method
+     * 
+     * @param jwt
+     * @return
+     * @throws JwtException
+     */
+    public Jws<Claims> verify(String jwt) throws JwtException {
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt);
+    }
+}
